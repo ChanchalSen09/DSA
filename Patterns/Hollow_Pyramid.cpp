91170bff57b7bff397aac4e174d19841d86ac395
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2025-02-23 14:26:09 +0000

    Some Code Change and Comments on it

--- Patterns/Hollow_Pyramid.cpp
+++ Patterns/Hollow_Pyramid.cpp
@@ -20,13 +20,13 @@ using namespace std;
 void Hollow_Pyramid(int n){
     for(int i = 0;i<n;i++){
         int k = 0;
-        int m =0;
         for(int j = 0;j<(n*2)-1;j++){
             if(j<(n-1)-i){
                 cout<<" ";
             }else if(k<2*i+1){
-             if(k==0||k==2*i ||i==n-1){
-                cout<<"*";}else{
+             if(k==0||k==2*i ||i==n-1){ // Last Line Handle on that Case i==n-1; 
+                cout<<"*";}
+                else{
                     cout<<" ";
                 }
             k++; 
@@ -34,13 +34,12 @@ void Hollow_Pyramid(int n){
                 cout<<" ";
             }
         }
-        m=k;
         cout<<endl;
     }
 }
 int main()
 {   
     // FullPyramid(6);
-     Hollow_Pyramid(6);
+     Hollow_Pyramid(5);
     return 0;
 }
