8323159fda24a43cf7a1158e0e4fe48f1c50019b
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2025-02-23 13:46:29 +0000

    Hollow_Pyramid Question Done

---
+++ Patterns/Hollow_Pyramid.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+// void FullPyramid(int n){
+//     for(int i = 0;i<n;i++){
+//         int k = 0;
+//         for(int j = 0;j<(n*2)-1;j++){
+//             if(j<(n-1)-i){
+//                 cout<<" ";
+//             }else if(k<2*i+1){
+//             cout<<"*";
+//             k++;
+//             }else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+// }
+void Hollow_Pyramid(int n){
+    for(int i = 0;i<n;i++){
+        int k = 0;
+        int m =0;
+        for(int j = 0;j<(n*2)-1;j++){
+            if(j<(n-1)-i){
+                cout<<" ";
+            }else if(k<2*i+1){
+             if(k==0||k==2*i ||i==n-1){
+                cout<<"*";}else{
+                    cout<<" ";
+                }
+            k++; 
+            }else{
+                cout<<" ";
+            }
+        }
+        m=k;
+        cout<<endl;
+    }
+}
+int main()
+{   
+    // FullPyramid(6);
+     Hollow_Pyramid(6);
+    return 0;
+}
