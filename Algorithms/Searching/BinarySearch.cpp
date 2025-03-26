69e71f8ecdd2a8e13ab19aa9263abd44fb0f4d5b
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2025-03-26 12:59:40 +0000

    Binary Seach Added

--- Algorithms/Searching/BinarySearch.cpp
+++ Algorithms/Searching/BinarySearch.cpp
@@ -2,7 +2,14 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-
+int BinarySearch(vector<int>arr,int target,int start,int end){
+  int mid = start+(end-start)/2;
+  if(arr[mid]==target){
+    return mid;
+  }else if (arr[mid]<target){   return BinarySearch(arr,target,mid,end);
+   
+  }else {return BinarySearch(arr,target,start,mid); }
+}
 int BinarySearch(vector<int>arr,int size,int target){
     int start = 0;
     int end = size - 1;
@@ -23,8 +30,8 @@ int main()
 {
     vector<int>arr={10,20,25,30,65,175,460,950};
     int size = arr.size();
-    int target = 10;
-    int answer = BinarySearch(arr,size,target);
+    int target = 460;
+    int answer = BinarySearch(arr,target,0,size);
     cout<<answer<<endl;
     return 0;
 }
