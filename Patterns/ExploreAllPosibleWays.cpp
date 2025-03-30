7389080c01d648e35ee9f1890205e6cc10e61ff2
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2025-03-30 18:53:32 +0000

    Question Added

--- Patterns/ExploreAllPosibleWays.cpp
+++ Patterns/ExploreAllPosibleWays.cpp
@@ -1,10 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
+class Solution {
+public:
+    int solve(vector<int>& nums, int size, int index) {
+        //base case
+        if(index >= size ) {
+            return 0;
+        }
 
-int main()
-{
-     
-    return 0;
-}
+        //chori karlo -> ith index pr
+        int option1 = nums[index] + solve(nums, size, index+2);
+
+        //chori mat karo  -> ith index pr
+        int option2 = 0 + solve(nums, size, index+1);
+
+        int finalAns = max(option1, option2);
+        return finalAns;
+    }
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        int index = 0;
+        int ans = solve(nums, size, index);
+        return ans;
+
+        
+    }
+};
