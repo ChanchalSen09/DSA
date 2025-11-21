be9468578faadc8b0cbe7d90861fd3616a43ea5e
Chanchal <92750541+ChanchalSen09@users.noreply.github.com>
2025-11-21 23:40:11 +0530

    permutation of string

---
+++ 04String/Permutation_In_String.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void PermutationofString(string str,int index){
+    if(index>=str.length()){
+        cout<<index<<" : "<<str<<" "<<endl;
+        return;
+    }
+    for(int j = index;j<str.length();j++){
+        swap(str[index],str[j]);
+        PermutationofString(str,index+1);
+        swap(str[index],str[j]);
+    }
+}
+int main()
+{
+     string str = "abc";
+     int index = 0;
+     PermutationofString(str,index);
+      return 0;
+}
