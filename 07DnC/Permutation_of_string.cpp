25f311751fd406563e0b404abf37b40cf7d3b756
Chanchal <92750541+ChanchalSen09@users.noreply.github.com>
2025-11-22 17:10:42 +0530

    Permutation Added

--- 07DnC/Permutation_of_string.cpp
+++ 07DnC/Permutation_of_string.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
 using namespace std;
-
+void PermutationofString(string str,int index){
+    if(index>=str.length()){
+        cout<<index<<" : "<<str<<" "<<endl;
+        return;
+    }
+    for(int j = index;j<str.length();j++){
+        swap(str[index],str[j]);
+        PermutationofString(str,index+1);
+        swap(str[index],str[j]);
+    }
+}
 int main()
 {
-     
-    return 0;
+     string str = "abcd";
+     int index = 0;
+     PermutationofString(str,index);
+      return 0;
 }
