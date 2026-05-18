81a89f76260f3fdb1375873b3c6e97230405fc34
chanchalsen09 <bkchanchalsen@gmail.com>
2026-05-18 15:41:33 +0530

    Add PostOrder and InOrder traversal implementations to binary tree

--- 11_Trees/Binary_Trees/pre_order.cpp
+++ 11_Trees/Binary_Trees/pre_order.cpp
@@ -19,12 +19,32 @@ void PreOrderRecursive(Node* root){
     PreOrderRecursive(root->right);
   
 }
+
+void PostOrderRecursive(Node* root){
+    if(root == nullptr) return;
+    PostOrderRecursive(root->left);
+    PostOrderRecursive(root->right);
+    cout<<root->data<<" ";
+  
+}
+void InOrderRecursive(Node* root){
+    if(root == nullptr) return;
+    InOrderRecursive(root->left);
+    cout<<root->data<<" ";
+    InOrderRecursive(root->right);
+}
 int main()
 {
      Node* root = new Node(4);
      root->left = new Node(10);
      root->right = new Node(12);
      PreOrderRecursive(root);
+     cout<<endl;
+     PostOrderRecursive(root);
+     cout<<endl;
+     InOrderRecursive(root);
+
+
      return 0;
 }
 // space o(n) time o(n)
