cf7393d196e43858cbf6cbf65bfd8844a2040799
chanchalsen09 <bkchanchalsen@gmail.com>
2026-07-14 21:44:42 +0530

    Implement RotateByK function to rotate an array by k steps

--- 03Array/01BasicArray/RotateByK.cpp
+++ 03Array/01BasicArray/RotateByK.cpp
@@ -12,31 +12,44 @@ You can assume k can be greater than the size of the array.
 Modify the array in place.
 */
 
-void rotateByK(vector<int>& nums, int k) {
-    int n = nums.size();
-    if (n == 0) {
-        return;
-    }
+// void rotateByK(vector<int>& nums, int k) {
+//     int n = nums.size();
+//     if (n == 0) {
+//         return;
+//     }
 
-    k %= n;
-    if (k == 0) {
-        return;
-    }
+//     k %= n;
+//     if (k == 0) {
+//         return;
+//     }
 
-    reverse(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.begin() + k);
-    reverse(nums.begin() + k, nums.end());
+//     reverse(nums.begin(), nums.end());
+//     reverse(nums.begin(), nums.begin() + k);
+//     reverse(nums.begin() + k, nums.end());
+// }
+
+void RotateByK(vector<int>&num,int k){
+    int size = num.size();
+    vector<int>ans;
+    for(int i = size-k;i<size;++i){
+        ans.push_back(num[i]);
+    }
+    for(int i = 0 ;i<size-k;i++){
+        ans.push_back(num[i]);
+    }
+    for(int k : ans){
+        cout<<k<<" ";
+    }
 }
-
 int main() {
-    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
-    int k = 3;
+    vector<int> nums = {-1,-100,3,99};
+    int k = 2;
 
-    rotateByK(nums, k);
+    RotateByK(nums, k);
 
-    for (int value : nums) {
-        cout << value << " ";
-    }
+    // for (int value : nums) {
+    //     cout << value << " ";
+    // }
 
     return 0;
 }
