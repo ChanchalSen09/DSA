cabf151298e4bb17cc50a7a3de9f67beeebb3708
chanchalsen09 <bkchanchalsen@gmail.com>
2026-07-14 18:48:00 +0530

    Refactor removeDuplicates function to accept size parameter and fix return value

--- 03Array/03_EasyArray/Remove_Duplicates_Inplace_From_Sorted_Array.cpp
+++ 03Array/03_EasyArray/Remove_Duplicates_Inplace_From_Sorted_Array.cpp
@@ -13,23 +13,28 @@ Return the number of unique elements `k`, where the first `k` positions of the
 array store the final answer.
 */
 
-int removeDuplicates(vector<int>& arr) {
-   int unique = 0;
-
-   for(int i = 0;i<arr.size();i++){
-    if(arr[i]!=arr[unique]){
-        unique++;
-        arr[unique]=arr[i];
+int removeDuplicates(vector<int>&arr,int size){
+    int unique = 0;
+    for(int i=1;i<size;i++){
+        if (arr[i]==arr[unique]){
+            continue;
+        }
+        if(arr[i]!=arr[unique]){
+            ++unique;
+            arr[unique] = arr[i];
+        }
     }
-   }
-    return unique+1;
+    return unique;
 }
 
 int main() {
     vector<int> arr = {1, 1, 2, 2, 2, 3, 3};
-
-    int k = removeDuplicates(arr);
-    cout << k << endl;
+   int size = arr.size();
+    int k = removeDuplicates(arr,size);
+    for(int i = 0;i<=k;i++){
+        cout<<arr[i]<<" ";
+    }
+    // cout << k << endl;
 
     return 0;
 }
